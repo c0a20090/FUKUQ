d6a68f310fb60ee987bfc93973fcdd2494f479a9
c0a20090 <68372697+c0a20090@users.noreply.github.com>
2023-02-06 22:26:07 +0000

    seedsの修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,24 @@
-# メインのサンプルユーザーを1人作成する
-User.create!(name:  "masaya",
-  email: "c0a200908d@edu.teu.ac.jp",
-  password:              "1234567",
-  password_confirmation: "1234567",
-  admin:     true,
-  activated: true,
-  activated_at: Time.zone.now)
+# # メインのサンプルユーザーを1人作成する
+# User.create!(name:  "masaya",
+#   email: "c0a200908d@edu.teu.ac.jp",
+#   password:              "1234567",
+#   password_confirmation: "1234567",
+#   admin:     true,
+#   activated: true,
+#   activated_at: Time.zone.now)
 
 # 追加のユーザーをまとめて生成する
-30.times do |n|
-  name  = Faker::Name.name
-  email = "example-#{n+1}@example.com"
-  password = "password"
-  User.create!(name:  name,
-               email: email,
-               password:              password,
-               password_confirmation: password,
-               activated: true,
-               activated_at: Time.zone.now)
-end
+# 30.times do |n|
+#   name  = Faker::Name.name
+#   email = "example-#{n+1}@example.com"
+#   password = "password"
+#   User.create!(name:  name,
+#                email: email,
+#                password:              password,
+#                password_confirmation: password,
+#                activated: true,
+#                activated_at: Time.zone.now)
+# end
 
 # ユーザーの一部を対象に質問を生成する
 users = User.order(:created_at).take(6)
